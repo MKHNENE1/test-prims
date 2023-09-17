e9f40d5e41005b1d8c9df738582287d9ab64d341
MKHNENE1 <102330949+MKHNENE1@users.noreply.github.com>
2023-09-17 09:34:39 +0300

    index.php

--- index.php
+++ index.php
@@ -1,3 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Your Page Title</title>
+<style>
+    body {
+  font-family: Arial, sans-serif;
+  background-color: #f2f2f2;
+  margin: 0;
+  padding: 20px;
+}
+
+h1 {
+  color: #333;
+}
+
+p {
+  color: #666;
+}
+
+</style>
+</head>
+<body>
+    <h1>Welcome to My Web Page</h1>
+<p>This is an example of a web page created with HTML and CSS.</p>
 <?php
 // Test Cases
 // clearstatcache();
@@ -7,10 +32,11 @@ function change_permissions($x)
     return "This Is Directory And Only Files Allowed<br>";
   } elseif (is_file($x)) {
     if (str_contains($x, ".txt")) {
-      // chmod($x, 0700);
-      // echo "<pre>";
-      // print_r(stat($x));
-      // echo "</pre>";
+       chmod($x, 0700);
+    //   echo "<pre>";
+    //   print_r(stat($x));
+    //   echo "</pre>";
+    //   echo $x . "<br>";
       return fileperms($x) . " Permissions Changed<br>";
     } else {
       return "File Extension Is Not txt<br>";
@@ -20,5 +46,10 @@ function change_permissions($x)
 }
 
 echo change_permissions("Elzero"); // This Is Directory And Only Files Allowed
-echo change_permissions("Work.docx"); // File Extension Is Not Txt
-echo change_permissions("Result.txt"); // Permissions Changed
+echo change_permissions("work.docx"); // File Extension Is Not Txt
+echo change_permissions("result.txt"); // Permissions Changed
+// echo "DONE";
+?>
+<!-- Your page content goes here -->
+</body>
+</html>
